5d5c1eefb03b06c13e4e48b30c817d0c9eeecf79
bfidel0 <bfidel0@users.noreply.github.com>
2022-02-17 12:10:24 -0800

    knapsack

--- dynamic_programming.py
+++ dynamic_programming.py
@@ -41,3 +41,50 @@ def lcs_dp(seq1, seq2):
             else:
                 table[i+1][j+1] = max(table[i][j+1], table[i+1][j])
     return table[-1][-1]
+
+
+# Knapsack Problem O(2^n)
+def max_profit_recur(weights, profits, capacity, idx=0):
+    if idx == len(weights):
+        return 0
+    elif weights[idx] > capacity:
+        return max_profit_recur(weights, profits, capacity, idx + 1)
+    else:
+        option1 = max_profit_recur(weights, profits, capacity, idx + 1)
+        option2 = profits[idx] + \
+            max_profit_recur(weights, profits, capacity-weights[idx], idx + 1)
+    return max(option1, option2)
+
+
+test0 = {
+    'input': {
+        'capacity': 165,
+        'weights': [23, 31, 29, 44, 53, 38, 63, 85, 89, 82],
+        'profits': [92, 57, 49, 68, 60, 43, 67, 84, 87, 72]
+    },
+    'output': 309
+}
+
+
+#x = max_profit_recur(**test0['input'])
+# print(x)
+
+
+# Dynamic Knapsack Problem O(N)
+def max_profit_dp(weights, profits, capacity):
+    n = len(weights)
+    # Creates matrix with extra row and column for computation purposes, beware of off by one
+    table = [[0 for _ in range(capacity + 1)] for _ in range(n + 1)]
+    for i in range(n):
+        for c in range(1, capacity+1):
+            # It's plus one because of the additional row and column in creating the matrix
+            if weights[i] > c:
+                table[i+1][c] = table[i][c]
+            else:
+                table[i+1][c] = max(table[i][c], profits[i] +
+                                    table[i][c-weights[i]])
+    return table[-1][-1]
+
+
+x = max_profit_dp(**test0['input'])
+print(x)
